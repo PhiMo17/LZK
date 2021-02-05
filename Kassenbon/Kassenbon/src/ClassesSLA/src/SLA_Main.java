35dd21fb6b628a58133c1f70fd08b5cbbc08c28a
PhiMo17 <71430112+PhiMo17@users.noreply.github.com>
2021-02-05 09:07:38 +0100

    Add files via upload

---
+++ Kassenbon/Kassenbon/src/ClassesSLA/src/SLA_Main.java
@@ -0,0 +1,17 @@
+
+public class SLA_Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Artikel NST = new Artikel(1, "New Silvan Turbo", 7.5, 20);
+		Artikel AuB = new Artikel(2, "Afrim und Bashkim", 6, 20);
+		Artikel DazH = new Artikel(3, "Daniel allein zu Haus", 4.5, 20);
+		Artikel CoDPC = new Artikel(4, "Call of Duty - Phillips War PC", 30, 20);
+		Artikel CoDPS4 = new Artikel(5, "Call of Duty - Phillips War PS4", 40, 20);
+		Artikel CnPC = new Artikel(6, "Cybernico 2077 PC", 40, 20);
+		Artikel CnPS4 = new Artikel(7, "Cybernico 2077 PS4", 50, 20);
+		
+		ShoppingCart.Begrüßung();
+	}
+
+}
